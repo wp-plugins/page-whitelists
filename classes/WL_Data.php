ce7e47b0485a0b20bb746ae6d9948c0b940acab4
corvidism <corvidism@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-19 21:21:21 +0000

    Can delete a whitelist and return whitelist by id.
    git-svn-id: https://plugins.svn.wordpress.org/page-whitelists/trunk@1049706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/WL_Data.php
+++ classes/WL_Data.php
@@ -72,25 +72,73 @@ class WL_Data {
 				'name' => $name,
 				'time' => $time
 				)
-			);
+			);//should't these be sql escaped?
 			if (!$success) {
 				throw new Exception("Table row could not be written.",0); //TODO translate
+			} else {
+				$id = $wpdb->insert_id;
+				WL_Dev::log('created whitelist '.$id.', '.$name);
+				return new WL_List($id,$name,$time);
 			}
-			$id = $wpdb->insert_id;
+			
 		} catch (Exception $e) {
 			WL_Dev::log($e->getMessage());
 			if ($e->getCode()==1) {return true;} else {return false;}
 			//THOUGHT: should, or shouldn't this return the existing list as WL_Object?
 		}		
 		
-		WL_Dev::log('created whitelist '.$id.', '.$name);
-		//return array('created',$id); //THIS SHOULD RETURN THE WL_List OBJECT
-		return new WL_List($id,$name,$time);
+		
 	}
 	
 	public function delete_whitelist($id) {
-		//delete whitelist by id
-		//remove all references to it in the junction table
+		global $wpdb;
+		try {
+			$row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $this->list_table WHERE id = %d",$id));
+			if ($row != NULL) {
+				$success = $wpdb->delete($this->list_table, array(
+					'id' => $id
+					)
+				);
+				if (!$success) {
+					throw new Exception("Whitelist couldn't be deleted from the database.", 3);
+				} else {
+					$query_args = array(
+						'orderby'=>'ID'
+					);
+					$user_query = new WP_User_Query($query_args);
+					$users = $user_query->results;
+					foreach($users as $user) {
+						$this->remove_whitelist_user($id, $user->data->ID);
+					}
+					
+					$roles = get_editable_roles();
+					foreach($roles as $role) {
+						$this->remove_whitelist_role($id, strtolower($role['name'])); //get_editable_roles returns names capitalized, but the get_role() function needs them lowercase. *eyeroll*
+					}
+					
+					return array(true,'');
+				}				
+			} else {
+				throw new Exception("Whitelist doesn't exist.", 2);
+			}
+		} catch (Exception $e) {
+			WL_Dev::log($e->getMessage());
+			switch ($e->getCode()) {
+				case 2:
+					$message = 'missing'; 
+					break;
+				case 3:
+					$message = 'database error'; 
+				
+				default:
+					$message = 'unknown error';
+					break;
+			}
+			return array(false,$message);
+			//??
+		}
+		
+		
 		
 	}
 	
@@ -108,11 +156,12 @@ class WL_Data {
 	public function add_whitelist_role($list_id,$role_name) {
 		 $role = get_role($role_name);
 		 $role->add_cap('edit_whitelist_'.$list_id);
+		 //shouldn't these return a value? true/false on success failure? how would I determine this?
 	}
 	
 	public function remove_whitelist_role($list_id, $role_name) {
 		$role = get_role($role_name);
-		 $role->add_cap('edit_whitelist_'.$list_id);
+		$role->remove_cap('edit_whitelist_'.$list_id);
 	}
 	
 	public function add_whitelist_user($list_id,$user_id) {
@@ -126,9 +175,27 @@ class WL_Data {
 	}
 	
 	
+	public function get_whitelist($id) {
+		//get single whitelist
+		global $wpdb;
+		try {
+			$row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $this->list_table WHERE id = %d",$id));
+			if ($row != NULL) {
+				$list = new WL_List($row->id,$row->name,$row->time);
+				return $list;
+			} else {
+				WL_Dev::log("Whitelist doesn't exist.");
+				return false;
+			}
+		} catch (Exception $e) {
+			WL_Dev::log($e->getMessage);
+			return false;
+		}
+		
+		
+	}
 	
-	
-	public function get_whitelists() {
+	public function get_all_whitelists() {
 		global $wpdb;
 		$query = "SELECT * FROM $this->list_table";
 		$raw_lists = $wpdb->get_results($query,ARRAY_A);
--- tests/test-WL_Data.php
+++ tests/test-WL_Data.php
@@ -4,11 +4,27 @@ class WL_Data_Test extends WP_UnitTestCase {
 	private $data;
 	
 	function __construct() {
-		
-		
 		$this->data = New WL_Data();
 		$this->data->initialize();
 	}
+	
+	//toolbox
+	
+	private function can_edit($user_id,$list) {
+		$has_cap = user_can($user_id,'edit_whitelist_'.$list->get_id());
+		return $has_cap;
+	}  
+	
+	private function role_can($role_name,$list) {
+		$role = get_role($role_name);
+		$role_can = array_key_exists('edit_whitelist_'.$list->get_id(), $role->capabilities);
+		return $role_can;
+	}
+	
+	
+	
+	//tests
+	
 	
 	function test_create_whitelist_single() {
 		$result = $this->data->create_whitelist("mango");	
@@ -16,27 +32,52 @@ class WL_Data_Test extends WP_UnitTestCase {
 	}
 	
 	function test_create_whitelist_multiples() {
-		$GLOBALS['silent'] = false;
+		//$GLOBALS['silent'] = false;
 		$this->data->create_whitelist("mango"); //mango once
 		$result = $this->data->create_whitelist("mango"); //mango twice
+		//$GLOBALS['silent'] = true;
 		$this->assertTrue($result); //no two mangoes!
-		$GLOBALS['silent'] = true;
+		
 	}
 	
-	function test_get_whitelists_count() {
+	function test_get_whitelist() {
+		$result = $this->data->create_whitelist("mango");
+		$list = $this->data->get_whitelist($result->get_id());
+		$this->assertInstanceOf('WL_List',$list);
+	}
+	
+	function test_get_all_whitelists_count() {
 		$this->data->create_whitelist("mango");
 		$this->data->create_whitelist("apple");
-		$array = $this->data->get_whitelists();
+		$array = $this->data->get_all_whitelists();
 		$this->assertCount(2,$array);
 	}
 	
-	function test_get_whitelists_type() {
+	function test_get_all_whitelists_type() {
 		$this->data->create_whitelist("mango");
 		$this->data->create_whitelist("apple");
-		$array = $this->data->get_whitelists();
+		$array = $this->data->get_all_whitelists();
 		foreach ($array as $list) {
 			$this->assertInstanceOf('WL_List',$list); //this will try every member of $array and fail on first that doesn't pass
 		}		
+	}
+	
+	
+	function test_get_roles() {
+		$roles_array = get_editable_roles();
+		foreach ($roles_array as $role_array) {
+			$role_object = get_role(strtolower($role_array['name']));
+			$this->assertInstanceOf('WP_Role',$role_object);
+			//$this->assertEquals()
+		}
+	}
+	
+	function test_delete_whitelist_success() {
+		$result = $this->data->create_whitelist("mango");
+		$GLOBALS['silent'] = false;
+		$success = $this->data->delete_whitelist($result->get_id());
+		$GLOBALS['silent'] = true;
+		$this->assertTrue($success[0]);	
 	}
 	
 	//user tests
@@ -58,15 +99,31 @@ class WL_Data_Test extends WP_UnitTestCase {
 		$result = $this->data->create_whitelist("mango");
 		$user_id = $this->factory->user->create();
 		$this->data->add_whitelist_user($result->get_id(),$user_id);
-		$this->assertTrue(user_can($user_id,'edit_whitelist_'.$result->get_id()));
+		$this->assertTrue($this->can_edit($user_id,$result));
 	}
 	
 	function test_remove_whitelist_user() {
 		$result = $this->data->create_whitelist("mango");
 		$user_id = $this->factory->user->create();
 		$this->data->add_whitelist_user($result->get_id(),$user_id);
-		$this->assertTrue(user_can($user_id,'edit_whitelist_'.$result->get_id()));
+		$this->assertTrue($this->can_edit($user_id,$result));
 		$this->data->remove_whitelist_user($result->get_id(),$user_id);
-		$this->assertFalse(user_can($user_id,'edit_whitelist_'.$result->get_id()));
+		$this->assertFalse($this->can_edit($user_id,$result));
+	}
+	
+	function test_add_whitelist_role() {
+		//$this->markTestSkipped('not doing this now');
+		$result = $this->data->create_whitelist("mango");
+		$this->data->add_whitelist_role($result->get_id(),'editor');
+		$this->assertTrue($this->role_can('editor',$result));
+	}
+	
+	function test_remove_whitelist_role() {
+		$result = $this->data->create_whitelist("mango");
+		$role_name = 'editor';
+		$this->data->add_whitelist_role($result->get_id(),$role_name);
+		$this->assertTrue($this->role_can($role_name,$result));
+		$this->data->remove_whitelist_role($result->get_id(),$role_name);
+		$this->assertFalse($this->role_can($role_name,$result));
 	}
 }
