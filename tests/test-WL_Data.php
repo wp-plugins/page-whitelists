baca75ab7d351567a47a341142beace09c56f95c
corvidism <corvidism@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-19 21:20:15 +0000

    WL_Data can add a custom capability "edit_whitelist_X" where X is a whitelist id.
    git-svn-id: https://plugins.svn.wordpress.org/page-whitelists/trunk@1049703 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/test-WL_Data.php
+++ tests/test-WL_Data.php
@@ -6,20 +6,17 @@ class WL_Data_Test extends WP_UnitTestCase {
 	function __construct() {
 		$this->data = New WL_Data();
 		$this->data->initialize();
-		//$this->dummy_user = new WP_User(1);
 	}
 	
-	
 	function test_create_whitelist_single() {
-		$result = $this->data->create_whitelist("mango");
-		//$this->assertEquals('created',$result[0]);
+		$result = $this->data->create_whitelist("mango");	
 		$this->assertInstanceOf('WL_List',$result);
 	}
 	
 	function test_create_whitelist_multiples() {
-			$this->data->create_whitelist("mango"); //mango once
-			$result = $this->data->create_whitelist("mango"); //mango twice
-			$this->assertTrue($result); //no two mangoes!
+		$this->data->create_whitelist("mango"); //mango once
+		$result = $this->data->create_whitelist("mango"); //mango twice
+		$this->assertTrue($result); //no two mangoes!
 	}
 	
 	function test_get_whitelists_count() {
@@ -36,5 +33,27 @@ class WL_Data_Test extends WP_UnitTestCase {
 		foreach ($array as $list) {
 			$this->assertInstanceOf('WL_List',$list); //this will try every member of $array and fail on first that doesn't pass
 		}		
+	}
+	
+	//user tests
+	
+	function test_user_exists() {
+		$user_id = $this->factory->user->create();
+		$user = get_user_by('id',$user_id);
+		$this->assertInstanceOf('WP_User',$user);
+	}
+	
+	function test_add_cap() {
+		$user_id = $this->factory->user->create();
+		$user = get_user_by('id',$user_id);
+		$user->add_cap('edit_whitelist');
+		$this->assertTrue(user_can($user->ID,'edit_whitelist'));
+	}
+	
+	function add_whitelist_user() {
+		$result = $this->data->create_whitelist("mango");
+		$user_id = $this->factory->user->create();
+		$this->data->add_whitelist_to_user($result->get_id(),$user_id);
+		$this->assertTrue(user_can($user_id,'edit_whitelist_'.$result->get_id()));
 	}
 }
