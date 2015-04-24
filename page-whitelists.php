681ebd2123c18ea82d4dca80c8e3547e44f6b9ae
corvidism <corvidism@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 20:13:38 +0000

    bumping version 2.0.1 (mangled tag 2.0)
    git-svn-id: https://plugins.svn.wordpress.org/page-whitelists/trunk@1145155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page-whitelists.php
+++ page-whitelists.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Page Whitelists
-Version: 2.0
+Version: 2.0.1
 Description: This plugin allows administrators to limit access only to selected pages - either for single users, or for entire roles (also works with roles created by other plugins).  
 Author: Anna Frankova
 Author URI: http://corvidism.com
